49b04a33b739d4a3deb00964403c838e1ab63019
Bonee94 <bonee94@yahoo.com>
2022-10-28 15:28:47 -0500

    Prompt and confirms added

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -7,8 +7,17 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+}
+
+function generatePassword() {
+  var passwordLength = prompt('What length of password would you like? \nPlease enter a number from 8-128');
+  var passwordUppercase = confirm('Would you like to use uppercase characters?');
+  var passwordLowercase = confirm('Would you like to use lowerscase characters?');
+  var passwordNumeric = confirm('Would you like to use numeric characters?');
+  var passworddSpecial = confirm('Lastly, would you like to use special characters?');
 
 }
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
